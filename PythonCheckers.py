8abee8b0f9c74bc64e61430ca126f67f186eaa89
Alistair Mckay <allymckay5@gmail.com>
2017-10-24 22:06:06 +0100

    Input validation complete, started AI stuff, made progress on kings

--- PythonCheckers.py
+++ PythonCheckers.py
@@ -4,7 +4,7 @@ END = '\033[0m'
 import copy
 board =[["      ","   1   ", "     2    ","    3    ","    4    ","   5   ", "      6   ","    7    ","    8    "], 
 		 	["   1 ", "|       |", "|       |","|       |", (RED + "|Counter|" + END), "|       |", (RED + "|Counter|" + END),"|       |", RED + "|Counter|" + END], 
-		 	["   2 " , "|       |", "|       |",(RED + "|Counter|" + END), "|       |", (RED + "|Counter|" + END), "|       |",(RED + "|Counter|" + END), "|       |"],
+		 	["   2 " , (BLUE + "|Counter|" + END), "|       |",(RED + "|Counter|" + END), "|       |", (RED + "|Counter|" + END), "|       |",(RED + "|Counter|" + END), "|       |"],
 		 	["   3 " , "|       |", "|       |","|       |", (RED + "|Counter|" + END), "|       |", (RED + "|Counter|" + END),"|       |", (RED + "|Counter|" + END)],
 		 	["   4 " , "|       |","|       |","|       |", "|       |","|       |","|       |", "|       |", "|       |"],
 		 	["   5 " , "|       |","|       |","|       |", (RED + "|Counter|" + END),"|       |","|       |", "|       |", "|       |"],
@@ -32,27 +32,23 @@ def userturnlogic(counterstaken, board):
 			try:
 				number = int(counter_to_move_input)
 			except ValueError:
-				print "Error"
+				print "Please enter a row number"
+				continue
+			except SyntaxError:
+				print "Please enter a row number"
 				continue
 			else:
 				counter_to_move = int(counter_to_move_input)
 				break
-		#if counter_to_move_string not in ["1","2","3","4","5","6","7","8"]:
-			#print "Not a valid input"
-			#print counter_to_move_string
-			#continue
-		#else:
-			#print "8"
-		#except:
-	 		#print("Please enter an integer value and not a string")
-	 		#continue
-	 	#try:
 	 	while True:
 			counter_to_movey_input = raw_input("Enter the column number of the counter you wish to move: ")
 			try:
 				numbery = int(counter_to_movey_input)
 			except ValueError:
-				print "Error"
+				print "Please enter a column number"
+				continue
+			except SyntaxError:
+				print "Please enter a column number"
 				continue
 			else:
 				counter_to_movey = int(counter_to_movey_input)
@@ -60,7 +56,7 @@ def userturnlogic(counterstaken, board):
 	 	#except:
 	 		#print("Please enter an integer value and not a string")
 	 		#continue
-	 	
+
  		if board[counter_to_move][counter_to_movey] == "|       |":
 			print"There is no counter in this position "
 			continue
@@ -69,19 +65,47 @@ def userturnlogic(counterstaken, board):
 			continue
 		else:
 			break
-	print counter_to_move
-	print counter_to_movey
 	valid_move = 0;
 	while valid_move == 0:
 		#try:
-		counter_to_moveto = int(input("Enter the row number of the position you wish to move to: "))
-		counter_to_movetoy = int(input("Enter the column number of the position you wish to move to : "))
+		while True:
+			counter_to_move_to_input = raw_input("Enter the row number of the counter you wish to move to: ")
+			try:
+				number2 = int(counter_to_move_to_input)
+			except ValueError:
+				print "Error"
+				continue
+			except SyntaxError:
+				print "Please enter a column number"
+				continue
+			else:
+				counter_to_moveto = int(counter_to_move_to_input)
+				break
+		while True:
+			counter_to_move_toy_input = raw_input("Enter the column number of the counter you wish to move to: ")
+			try:
+				number3 = int(counter_to_move_toy_input)
+			except ValueError:
+				print "Error"
+				continue
+			except SyntaxError:
+				print "Please enter a column number"
+				continue
+			else:
+				counter_to_movetoy = int(counter_to_move_toy_input)
+				break
 		#except:
 			#print("Please enter an integer value and not a string")
 			#throw
+		board[counter_to_move][counter_to_movey]
+		#kinglogic(counter_to_move, counter_to_movey, counter_to_moveto, counter_to_movetoy)
+		print board[counter_to_move][counter_to_movey]
 		a = (counter_to_move,counter_to_movey) 
 		b = (counter_to_moveto,counter_to_movetoy)
 		displacement = (a[0] - b[0], a[1] - b[1])
+		#if board[counter_to_move][counter_to_movey] == (BLUE + "|King   |" + END):
+	 		#kinglogic()
+	 		#valid_move = 1
 		if board[counter_to_moveto][counter_to_movetoy] == BLUE + "|Counter|" + END or board[counter_to_moveto][counter_to_movetoy] == RED + "|Counter|" + END:
 			print"There is already a counter in this position"
 			continue
@@ -93,7 +117,7 @@ def userturnlogic(counterstaken, board):
 					counterstaken = counterstaken + 1
 					valid_move = 1;
 				elif board[counter_to_moveto+1][counter_to_movetoy+1] != RED + "|Counter|" + END:
-					print "Not a valid move"
+					print "Not a valid move-"
 					continue
 		if displacement == (2,2):
 			if board[counter_to_moveto+1][counter_to_movetoy+1] == RED + "|Counter|" + END:
@@ -103,22 +127,22 @@ def userturnlogic(counterstaken, board):
 				counterstaken = counterstaken + 1
 				valid_move = 1;	
 			elif board[counter_to_moveto+1][counter_to_movetoy+1] != RED + "|Counter|" + END:
-				print "Not a valid move"
+				print "Not a valid move--"
 				continue
 		if board[counter_to_moveto+1][counter_to_movetoy-1] == RED + "|Counter|" + END:
 			if counter_to_moveto is counter_to_move - 2 or counter_to_movetoy is counter_to_movey - 2  :
-				print"Not a valid move"
+				print"Not a valid move---"
 				continue
 		if counter_to_moveto == counter_to_move - 1 and counter_to_movetoy == counter_to_movey:
-			print "Not a valid move"
+			print "Not a valid move----"
 			continue
 		#elif counter_to_moveto is not counter_to_move - 1 or counter_to_movetoy is  counter_to_movey  :
 		#	print"Not a valid move"
 		if board[counter_to_moveto][counter_to_movetoy] == "      " or board[counter_to_moveto][counter_to_movetoy] == "   1   " or board[counter_to_moveto][counter_to_movetoy] == "     2    " or board[counter_to_moveto][counter_to_movetoy] == "    3    " or board[counter_to_moveto][counter_to_movetoy] == "    4    " or board[counter_to_moveto][counter_to_movetoy] == "   5   " or board[counter_to_moveto][counter_to_movetoy] == "      6   " or board[counter_to_moveto][counter_to_movetoy] == "    7    " or board[counter_to_moveto][counter_to_movetoy] == "    8    ":
-			print"Not a valid move" 
+			print"Not a valid move-----" 
 			continue
 		if counter_to_move != counter_to_moveto +1: 
-			print"Not a valid move" 
+			print"Not a valid move------" 
 			continue
 		else:
 			valid_move = 1;
@@ -127,13 +151,11 @@ def userturnlogic(counterstaken, board):
 			#counterstaken = counterstaken + 1
 	reversedisplacement = (displacement[0], displacement[1])
 	if counter_to_moveto == 1:
-		board[counter_to_moveto][counter_to_movetoy] = BLUE + "|  King |" + END
+		board[counter_to_moveto][counter_to_movetoy] = BLUE + "|King   |" + END
+	
 	print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
       	for row in board]))
 	print "counters taken =: " ,counterstaken
-	
-	print counter_to_moveto
-	print counter_to_movetoy
 	while True:
 		undo = raw_input("Undo? Y/N: ")
 		if undo in ['y', 'Y']:
@@ -209,12 +231,25 @@ def userturnlogic(counterstaken, board):
 	     			for row in board]))
 				print "counters taken =: " ,counterstaken
 				break
+	
 	#if counter_to_moveto == 1:
-def kinglogic():
+def kinglogic(counter_to_move, counter_to_movey, counter_to_moveto, counter_to_movetoy):
 	board[counter_to_move][counter_to_movey] = "|       |"
 	board[counter_to_moveto][counter_to_movetoy] = BLUE + "|King   |" + END
-def AIturnlogic():
-	print"AI TURN LOGIC GOES HERE"
+	print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
+	     			for row in board]))
+def AIturnlogic(board):
+	#board.index((RED + "|Counter|" + END))
+	indices = [i for i, x in enumerate(board) if x == "    4    "]
+	#print board[1][4]
+	#print "k"
+	#print indices
+	#print "AI TURN LOGIC GOES HERE"
+	#print any((RED + "|Counter|" + END) in sublist for sublist in board)
+	#for record in board:
+		#for data in record:
+    			#if data == (RED + "|Counter|" + END):
+        			#print data
 def youwin():
 	text = [["|  | |  ||       ||  | |  |  | | _ | ||   | |  |  | ||  |"], 
 			["|  |_|  ||   _   ||  | |  |  | || || ||   | |   |_| ||  |"], 
@@ -264,6 +299,5 @@ while True:
 
 while counterstaken == 0:
 	userturnlogic(counterstaken, board)
-	AIturnlogic()
-	print counterstaken
+	AIturnlogic(board)
 youwin()
